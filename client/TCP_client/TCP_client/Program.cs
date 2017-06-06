056e728fec6c14e393697b1b94ddb7583f57b0ff
Tim Wanders <timwanders241@gmail.com>
2017-06-06 20:08:19 +0200

    First commit

---
+++ client/TCP_client/TCP_client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace TCP_client
+{
+	class MainClass
+	{
+		internal static TcpClient TCP;
+		internal static Stream TCPStream;
+		internal static byte[] Streambyte;
+		internal static byte[] TCPbyte;
+		internal static int TCPint;
+		internal static void Receive()
+		{
+				while (true)
+				{
+					TCPbyte = new byte[100];
+					TCPint = TCPStream.Read(TCPbyte, 0, 100);
+					Console.Write(Encoding.ASCII.GetString(TCPbyte) + Environment.NewLine);
+				}
+		}
+		internal static void Connect(string ip, int port)
+		{
+			TCP = new TcpClient();
+			TCP.Connect(IPAddress.Parse(ip), port);
+			TCPStream = TCP.GetStream();
+			StreamSend("Client's hostname: " + Dns.GetHostName() + Environment.NewLine + "Client's OS: " + Environment.OSVersion.Platform);
+			Receive();
+		}
+		internal static void StreamSend(string message)
+		{
+			Streambyte = Encoding.ASCII.GetBytes(message);
+			TCPStream.Write(Streambyte, 0, Streambyte.Length);
+		}
+		public static void Main(string[] args)
+		{	
+			try
+			{
+				Connect("127.0.0.1", 8001);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Connection error!");
+				Thread.Sleep(5000);
+				Main(args);
+			}
+		}
+	}
+}
---
+++ host/TCP_host/TCP_host/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+namespace TCP_host
+{
+	class MainClass
+	{
+		internal static TcpListener TcpListen;
+		internal static Socket TCPSocket;
+		internal static byte[] receivebytes;
+		internal static int SocketReceive;
+		internal static int TCPport;
+		internal static void Connect()
+		{
+			TcpListen = new TcpListener(IPAddress.Any, TCPport);
+			TcpListen.Start();
+		}
+		internal static void Listen(int port)
+		{
+			TCPport = port;
+			Connect();
+			Console.WriteLine("Server port: " + port);
+			Console.WriteLine("Waiting for client to connect");
+			Accept();
+		}
+		internal static void StreamSend(string message)
+		{
+			TCPSocket.Send(Encoding.ASCII.GetBytes(message));
+		}
+		internal static void Accept()
+		{
+			while (true)
+			{
+				try
+				{
+					TCPSocket = TcpListen.AcceptSocket();
+					Console.WriteLine("Connection accepted: " + TCPSocket.RemoteEndPoint);
+					Receive();
+					TCPinput();
+				}
+				catch (Exception)
+				{
+					Stop();
+					Console.WriteLine("Client has disconnected");
+					Connect();
+					Accept();
+				}
+			}
+		}
+		internal static void TCPinput()
+		{
+			string input;
+			while ((input = Console.ReadLine()) != null)
+			{
+				StreamSend(input);
+			}
+		}
+		internal static void Receive()
+		{
+				receivebytes = new byte[100];
+				SocketReceive = TCPSocket.Receive(receivebytes);
+				for (int i = 0; i < SocketReceive; i++)
+					Console.Write(Convert.ToChar(receivebytes[i]));
+				Console.Write(Environment.NewLine);
+		}
+		internal static void PING()
+		{
+			StreamSend("PING");
+		}
+		internal static void Stop()
+		{
+			TCPSocket.Close();
+			TcpListen.Stop();
+		}
+		public static void Main(string[] args)
+		{
+			try
+			{
+				Listen(8001);
+			}
+			catch (Exception e)
+			{
+				Stop();
+				Console.WriteLine("Error: " + e.ToString());
+				Main(args);
+			}
+		}
+	}
+}
